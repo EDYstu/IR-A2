89d9148a50227fa50688af368f2ae7ea9af4bee2
EDYstu <160554375+EDYstu@users.noreply.github.com>
2024-10-17 21:48:27 +1100

    Fixed orientation for Thor.
    Had to updated .m file too, issues with axis previously.

--- @Thor/Thor.m
+++ @Thor/Thor.m
@@ -1,49 +1,52 @@
 classdef Thor < RobotBaseClass
-    %% Thor on a non-standard linear rail created by a student
+    %% UR3e Universal Robot 3kg payload robot model
+    %
+    % WARNING: This model has been created by UTS students in the subject
+    % 41013. No guarentee is made about the accuracy or correctness of the
+    % of the DH parameters of the accompanying ply files. Do not assume
+    % that this matches the real robot!
 
-    properties(Access = public)              
+    properties(Access = public)   
         plyFileNameStem = 'Thor';
     end
     
     methods
-%% Define robot Function 
-        function self = Thor(baseTr)
-			self.CreateModel();
-            if nargin < 1			
-				baseTr = eye(4);				
+%% Constructor
+        function self = Thor(baseTr,useTool,toolFilename)
+            if nargin < 3
+                if nargin == 2
+                    error('If you set useTool you must pass in the toolFilename as well');
+                elseif nargin == 0 % Nothing passed
+                    baseTr = transl(0,0,0);  
+                end             
+            else % All passed in 
+                self.useTool = useTool;
+                toolTrData = load([toolFilename,'.mat']);
+                self.toolTr = toolTrData.tool;
+                self.toolFilename = [toolFilename,'.ply'];
             end
-            self.model.base = self.model.base.T * baseTr * trotx(pi/2) * troty(pi/2);
+          
+            self.CreateModel();
+			self.model.base = self.model.base.T * baseTr;
+            self.model.tool = self.toolTr; 
+            % q(2) = pi/2;
+            self.model.teach;
             
-            self.PlotAndColourRobot();         
+            self.PlotAndColourRobot();
+            
+            drawnow
         end
 
-%% Create the robot model
-        function CreateModel(self)   
-            % Create the Thor model
-            %ALL LENGTH VALUES NEED TO BE FOUND.
-            link(1) = Link([pi     0       0       pi/2    1]); % PRISMATIC Link
-            link(2) = Link('d',0.202,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
-            link(3) = Link('d',0,'a',0.16,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
-            link(4) = Link('d',0,'a',0,'alpha',pi/2,'qlim', deg2rad([-360 360]), 'offset', 0);
-            link(5) = Link('d',0.195,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
-            link(6) = Link('d',0,'a',0,'alpha',pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
-            link(6) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset',0);
-           
-            
-            % Incorporate joint limits
-            link(1).qlim = [-0.8 -0.01];
-            link(2).qlim = [-180 180]*pi/180; 
-            link(3).qlim = [-90 90]*pi/180; 
-            link(4).qlim = [-170 170]*pi/180; 
-            link(5).qlim = [-360 360]*pi/180; 
-            link(6).qlim = [-360 360]*pi/180; 
-            
-        
-            % link(3).offset = -pi/2;
-            % link(5).offset = -pi/2;
-            
+%% CreateModel
+        function CreateModel(self)
+            link(1) = Link('d',0.202,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0 );
+            link(2) = Link('d',0,'a',-0.16,'alpha',0,'qlim', deg2rad([-90 90]), 'offset',-pi/2);
+            link(3) = Link('d',0,'a',0,'alpha',-pi/2, 'qlim', deg2rad([-90 90]), 'offset',-pi/2);
+            link(4) = Link('d',-0.195,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
+            link(5) = Link('d',0,'a',0,'alpha',-pi/2,'qlim',deg2rad([-90 90]), 'offset',0);
+            link(6) = Link('d',    0,'a',0,'alpha',0,'qlim',deg2rad([-360 360]), 'offset', 0);
+
             self.model = SerialLink(link,'name',self.name);
-        end
-     
+        end  
     end
 end
