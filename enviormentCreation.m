a9a3ee2e05ce98fb5fa0e8bd866d331b0c518f64
EDYstu <160554375+EDYstu@users.noreply.github.com>
2024-10-18 18:06:50 +1100

    Additional Enviornment Files + updated environment
    Fixed meshes, scale and placed positioning in example environment.
    Having issues finding a light curtain model.

--- enviormentCreation.m
+++ enviormentCreation.m
@@ -29,7 +29,7 @@ function [ur3Robot] = enviormentCreation()
     verticesFire = get(fireEx,'Vertices');
     transformedVerticesFire = [verticesFire,ones(size(verticesFire,1),1)] * transl(3,-1,0)';
     set(fireEx,'Vertices',transformedVerticesFire(:,1:3));
-            
+
     person = PlaceObject('personMaleConstruction.ply');
     verticesPerson = get(person,'Vertices');
     transformedVerticesPerson = [verticesPerson,ones(size(verticesPerson,1),1)] * transl(3,0,0)';
@@ -41,14 +41,50 @@ function [ur3Robot] = enviormentCreation()
     transformedsEstopVertices1 = [estopV,ones(size(estopV,1),1)] * trotx(-pi/2) *transl(0.8,0.6,0.55)';
     set(estop,'Vertices',transformedsEstopVertices1(:,1:3));
 
-    %RedBin
-    redBin = PlaceObject('soda can- 350ml.ply'); 
-    redBinV = get(redBin,'Vertices');
-    transformedRedBinVertices = [redBinV,ones(size(redBinV,1),1)] * transl(1.5,0,1)';
-    set(redBin,'Vertices',transformedRedBinVertices(:,1:3));
+    %RedCan
+    redCan = PlaceObject('soda can- 350ml.ply'); 
+    redCanV = get(redCan,'Vertices');
+    transformedRedCanVertices = [redCanV,ones(size(redCanV,1),1)] * transl(0.8,0,0.625)';
+    set(redCan,'Vertices',transformedRedCanVertices(:,1:3));
+
+    %Milkcarton
+    mcarton = PlaceObject('milkcarton.ply'); 
+    mcartonV = get(mcarton,'Vertices');
+    transformedmcartonVertices = [mcartonV,ones(size(mcartonV,1),1)] * transl(0.8,-0.2,0.715)';
+    set(mcarton,'Vertices',transformedmcartonVertices(:,1:3)); %%Error upon this line
 
     zlim([0, 3]); % limit the figure
-    xlim([-4, 4]);
-    ylim([-3, 3]);
+    xlim([-2, 2]);
+    ylim([-1.5, 1.5]);
+
+    %Apple
+    Apple = PlaceObject('Apple.ply'); 
+    AppleV = get(Apple,'Vertices');
+    transformedAppleVertices = [AppleV,ones(size(AppleV,1),1)] * transl(0.8,0.2,0.575)';
+    set(Apple,'Vertices',transformedAppleVertices(:,1:3)); 
+
+    %RedBin
+    redBin = PlaceObject('RedDeskTrashBin.ply'); 
+    redBinV = get(redBin,'Vertices');
+    transformedRedBinVertices = [redBinV,ones(size(redBinV,1),1)]* trotz(pi/2) * transl(-1.2,0,0.5)';
+    set(redBin,'Vertices',transformedRedBinVertices(:,1:3));
+
+    %BlueBin
+    BlueBin = PlaceObject('BlueDeskTrashBin.ply'); 
+    BlueBinV = get(BlueBin,'Vertices');
+    transformedBlueBinVertices = [BlueBinV,ones(size(BlueBinV,1),1)]* trotz(pi/2) * transl(-1.2,0.4,0.5)';
+    set(BlueBin,'Vertices',transformedBlueBinVertices(:,1:3));
+
+    %GreenBin
+    GreenBin = PlaceObject('GreenDeskTrashBin.ply'); 
+    GreenBinV = get(GreenBin,'Vertices');
+    transformedGreenBinVertices = [GreenBinV,ones(size(GreenBinV,1),1)]* trotz(pi/2) * transl(-1.2,-0.4,0.5)';
+    set(GreenBin,'Vertices',transformedGreenBinVertices(:,1:3));
+
+
+
+    zlim([0, 3]); % limit the figure
+    xlim([-4.5, 4.5]);
+    ylim([-2.5, 2.5]);
 
 end
