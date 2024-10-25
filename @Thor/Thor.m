92a782f177c922f23710431e647dc7dbb2401957
EDYstu <160554375+EDYstu@users.noreply.github.com>
2024-10-25 11:45:03 +1100

    Updated for correct end effector placement

--- @Thor/Thor.m
+++ @Thor/Thor.m
@@ -1,5 +1,5 @@
 classdef Thor < RobotBaseClass
-    %% UR3e Universal Robot 3kg payload robot model
+    %% THOR Open Source 6DOF robot model
     %
     % WARNING: This model has been created by UTS students in the subject
     % 41013. No guarentee is made about the accuracy or correctness of the
@@ -28,9 +28,9 @@ classdef Thor < RobotBaseClass
           
             self.CreateModel();
 			self.model.base = self.model.base.T * baseTr;
-            self.model.tool = self.toolTr; 
+            self.model.tool = transl(0, 0, 0.11); %self.toolTr
             % q(2) = pi/2;
-            self.model.teach;
+            % self.model.teach;
             
             self.PlotAndColourRobot();
             
@@ -44,7 +44,7 @@ classdef Thor < RobotBaseClass
             link(3) = Link('d',0,'a',0,'alpha',-pi/2, 'qlim', deg2rad([-90 90]), 'offset',-pi/2);
             link(4) = Link('d',-0.195,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
             link(5) = Link('d',0,'a',0,'alpha',-pi/2,'qlim',deg2rad([-90 90]), 'offset',0);
-            link(6) = Link('d',    0,'a',0,'alpha',0,'qlim',deg2rad([-360 360]), 'offset', 0);
+            link(6) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([-360 360]), 'offset', 0);
 
             self.model = SerialLink(link,'name',self.name);
         end  
