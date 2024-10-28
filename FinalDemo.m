1de4c95538c6c5aa5b301212eedd9021b8e865be
Connor-Mawer <connormawer@gmail.com>
2024-10-29 10:15:15 +1100

    transfered and the movement

--- FinalDemo.m
+++ FinalDemo.m
@@ -1,16 +1,25 @@
 classdef FinalDemo < handle
 
     properties
+        steps = 50;
+
         %table Models Heights
         tableHeight = 0.54; 
         
         %trash brick starting locations
-        rcaniPos = [0.8,0,0.625];
-        rcaniMpos = [0.8,0,0.625];
-        rcanr2fMpos = [-0.75,0,0.7];
-        rcanDropPos = [-0.75,0,0.4];
+        rcanIpos = [0.8,0,0.625];
+        
 
         mcartoniPos = [0.8,-0.2,0.715];
+
+        %middle location
+        rcanMpos = [0.165,0,0.625];
+
+        %DropOff Locations
+        rcanFpos = [-0.75,0,0.7];
+
+        %Disposal locations
+        rcanDropPos = [-0.75,0,0.4];
 
         %trash model vertices
         redCanV;
@@ -19,9 +28,7 @@ classdef FinalDemo < handle
         %trash Models
         redCan;
         mcarton;
-            
-        %DropOff Locations
-        rcanfMpos = [0.165,0,0.625];
+           
 
 
         %robot
@@ -44,7 +51,7 @@ classdef FinalDemo < handle
             self.ur3 = UR3e(transl(-0.4,0,0.5));
             
             input('Press enter to begin moving')
-            self.move() %movex brick to locations 
+            self.moveTrash() %movex brick to locations 
            
            
 
@@ -117,7 +124,7 @@ classdef FinalDemo < handle
             %RedCan
             self.redCan = PlaceObject('soda can- 350ml.ply'); 
             self.redCanV = get(self.redCan,'Vertices');
-            transformedRedCanVertices = [self.redCanV,ones(size(self.redCanV,1),1)] * transl(self.rcaniPos)';
+            transformedRedCanVertices = [self.redCanV,ones(size(self.redCanV,1),1)] * transl(self.rcanIpos)';
             set(self.redCan,'Vertices',transformedRedCanVertices(:,1:3));
         
             %Milkcarton
@@ -138,9 +145,122 @@ classdef FinalDemo < handle
 
 
 
-            zlim([0, 4]); % limit the figure
-            xlim([-5, 5]);
-            ylim([-3, 3]);
+            scale = 0.5;
+            view(3)
+
+        end
+
+        function moveTrash(self)
+
+            %%Thor Transforms
+            qr1start = self.thor.model.getpos();
+
+            % T1 = transl(self.rcaniMpos) * trotx(pi);
+            T1 = transl(self.rcanIpos) * trotx(pi); %"where red can is"
+            q1=  self.thor.model.ikcon(T1); %q1 = r.model.ikcon(T1);
+
+            T2 = transl(self.rcanMpos); %* trotx(pi) * trotz(pi/2);
+            q2 = self.thor.model.ikcon(T2); %"Where to place can for pick up
+
+            T3 = transl((self.mcartoniPos) + [0.02,-0.02,0.03])* trotx(pi) * trotz(pi/2);
+            q3=  self.thor.model.ikcon(T3);
+
+
+            %spawn to red can
+            qMatrix1 = jtraj(qr1start,q1,self.steps);
+            %drop off
+            qMatrix2 = jtraj(q1,q2,self.steps);
+            %to Mcarton
+            qMatrix3 = jtraj(q2,q3,self.steps);
+
+            
+
+            %%UR3e Transforms.
+            qr2start = self.ur3.model.getpos();
+            
+            %0.165,0,0.625 [self.rcanfMpos] 
+            %%0.02 offset (x axis) from the object origin will work.
+            T1r2 = transl(self.rcanMpos + [-0.02, 0, 0])* trotx(pi/2) * troty(pi/2); %"where red can is"
+            q1r2=  self.ur3.model.ikcon(T1r2, qr2start); %q1 = r.model.ikcon(T1);
+            
+            qr2Matrix1 = jtraj(qr2start,q1r2,self.steps);
+
+            T2r2 = transl(self.rcanFpos + [-0.02, 0, 0])* trotx(pi/2) * troty(pi/2); %
+            q2r2=  self.ur3.model.ikcon(T2r2, self.ur3.model.getpos());
+
+            qr2Matrix2 = jtraj(q1r2,q2r2,self.steps);
+
+
+            %%Object Drop Transforms::
+            T_start = transl(self.rcanFpos);       % Initial Cartesian position
+            T_end = transl(self.rcanDropPos); % Final Cartesian position
+            T_trajectory = ctraj(T_start, T_end, self.steps);  % Cartesian trajectory for steps
+
+
+
+
+            %%Animate Transofrms.
+
+            %%Thor to Red Can
+            for i = 1:self.steps
+            self.thor.model.animate(qMatrix1(i,:));
+            drawnow();
+            pause(0.02);
+            end
+            
+            %Red Can placement THOR
+            for i = 1:self.steps
+                q1=  self.thor.model.ikcon(T1, q1);
+                qMatrix2 = jtraj(q1,q2,self.steps);
+                self.thor.model.animate(qMatrix2(i,:));
+
+                tr = self.thor.model.fkine(qMatrix2(i,:));
+                tvredCanV = [self.redCanV,ones(size(self.redCanV,1),1)]* trotx(pi)  * tr.T'; %
+                set(self.redCan,'Vertices',tvredCanV(:,1:3));
+            
+                drawnow();
+                pause(0.02);
+            end
+
+            %%Thor to Carton + UR3e to Can.
+            for i = 1:self.steps
+                self.thor.model.animate(qMatrix3(i,:)); %Thor to mcarton
+                %%Ur3e movement:
+                self.ur3.model.animate(qr2Matrix1(i,:));
+                q1r2=  self.ur3.model.ikcon(T1r2, self.ur3.model.getpos());
+                qr2Matrix1 = jtraj(self.ur3.model.getpos(),q1r2,self.steps);
+                drawnow();
+                pause(0.02);
+            
+            end
+
+            %UR3e drop can off.
+            for i = 1:self.steps
+                q2r2=  self.ur3.model.ikcon(T2r2, self.ur3.model.getpos());
+                qr2Matrix2 = jtraj(self.ur3.model.getpos(),q2r2,self.steps);
+                self.ur3.model.animate(qr2Matrix2(i,:));
+                
+
+                tr = self.ur3.model.fkine(qr2Matrix2(i,:));
+                tvredCanV = [self.redCanV,ones(size(self.redCanV,1),1)]* trotx(pi/2) * troty(pi/2) * tr.T'; %
+                set(self.redCan,'Vertices',tvredCanV(:,1:3));
+
+                drawnow();
+                pause(0.02);
+            end
+            %Can falling into bin
+            for i = 1:self.steps
+                % Update can's position based on the trajectory
+                trCan = T_trajectory(:,:,i);  % Get the transformation at each step
+                transformedRedCanVertices = [self.redCanV, ones(size(self.redCanV,1), 1)] * trCan';
+                
+                set(self.redCan, 'Vertices', transformedRedCanVertices(:, 1:3));  % Update can's position
+                drawnow();
+                pause(0.02);  % Adjust speed if needed
+            end
+
+            
+            
 
         end
 
